655d68f37e40335940389f94984350388a102aac
Bogdan Despotov <bogdan.despotov@cloudsigma.com>
2019-05-31 10:11:21 +0300

    Adding georeplication methods

--- src/api_georep.py
+++ src/api_georep.py
@@ -1,39 +1,39 @@
-from utils import auth
-from flask import Blueprint
+from utils import auth, gluster_cmd_to_http_response
+from gluster.cli import georep
+from flask import Blueprint, request
 
 georep_api = Blueprint('georep_api', __name__)
 
 
-@georep_api.route("/<mastervol>/<slaveuser>/<slavehost>/<slavevol>",
-                  methods=["PUT"])
+@georep_api.route("/<mastervol>/<slavehost>/<slavevol>", methods=["POST"])
 @auth
-def api_georep_create(mastervol, slaveuser, slavehost, slavevol):
-    pass
+def api_georep_create(mastervol, slavehost, slavevol):
+    data = request.get_json()
+    return gluster_cmd_to_http_response(georep.create, mastervol, slavehost, slavevol, **data)
 
 
-@georep_api.route("/<mastervol>/<slaveuser>/<slavehost>/<slavevol>/start",
-                  methods=["POST"])
+@georep_api.route("/<mastervol>/<slavehost>/<slavevol>/start", methods=["POST"])
 @auth
-def api_georep_start(mastervol, slaveuser, slavehost, slavevol):
-    pass
+def api_georep_start(mastervol, slavehost, slavevol):
+    data = request.get_json()
+    return gluster_cmd_to_http_response(georep.start, mastervol, slavehost, slavevol, **data)
 
 
-@georep_api.route("/<mastervol>/<slaveuser>/<slavehost>/<slavevol>/stop",
-                  methods=["POST"])
+@georep_api.route("/<mastervol>/<slavehost>/<slavevol>/stop", methods=["POST"])
 @auth
-def api_georep_stop(mastervol, slaveuser, slavehost, slavevol):
-    pass
+def api_georep_stop(mastervol, slavehost, slavevol):
+    data = request.get_json()
+    return gluster_cmd_to_http_response(georep.stop, mastervol, slavehost, slavevol, **data)
 
 
-@georep_api.route("/<mastervol>/<slaveuser>/<slavehost>/<slavevol>",
-                  methods=["DELETE"])
+@georep_api.route("/<mastervol>/<slavehost>/<slavevol>", methods=["DELETE"])
 @auth
-def api_georep_delete(mastervol, slaveuser, slavehost, slavevol):
-    pass
+def api_georep_delete(mastervol, slavehost, slavevol):
+    data = request.get_json()
+    return gluster_cmd_to_http_response(georep.delete, mastervol, slavehost, slavevol, **data)
 
 
-@georep_api.route("/<mastervol>/<slaveuser>/<slavehost>/<slavevol>/config",
-                  methods=["GET"])
+@georep_api.route("/<mastervol>/<slaveuser>/<slavehost>/<slavevol>/config", methods=["GET"])
 @auth
 def api_georep_config_get(mastervol, slaveuser, slavehost, slavevol):
     pass
@@ -57,12 +57,10 @@ def api_georep_config_reset(mastervol, slaveuser, slavehost, slavevol):
 # without Volume names
 @georep_api.route("", methods=["GET"])
 @georep_api.route("/<mastervol>", methods=["GET"])
-@georep_api.route("/<mastervol>/<slaveuser>/<slavehost>/<slavevol>",
-                  methods=["GET"])
+@georep_api.route("/<mastervol>/<slaveuser>/<slavehost>/<slavevol>", methods=["GET"])
 @auth
-def api_georep_status(mastervol=None, slaveuser=None, slavehost=None,
-                      slavevol=None):
-    pass
+def api_georep_status(mastervol=None, slaveuser=None, slavehost=None, slavevol=None):
+    return gluster_cmd_to_http_response(georep.status, mastervol, slaveuser, slavehost, slavevol)
 
 
 @georep_api.route("/<mastervol>/<slaveuser>/<slavehost>/<slavevol>/checkpoint",
